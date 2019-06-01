e81ecc6e1f12952cac1d6fac5a343e3b53d99b39
Stéphane Laviroétte <stephane@lavirotte.com>
2019-06-01 16:07:11 +0200

    Suppress requires sleep module in test file (so node.js minimum release is 9.3)

--- examples/grove_4_digit_display.js
+++ examples/grove_4_digit_display.js
@@ -1,6 +1,13 @@
 const Grove4DigitDisplay = require('../grove_4_digit_display.js');
 const strftime = require('strftime');
-const time = require('sleep');
+
+function msleep(n) {
+  Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
+}
+
+function sleep(n) {
+  msleep(n*1000);
+}
 
 function main() {
 	var count, display,	pin, t;
@@ -11,17 +18,17 @@ function main() {
 
 	for (var i = 10; i >= 0; i--) {
 		display.show(i);
-		time.msleep(200);
+		msleep(200);
 	}
-	time.msleep(500);
+	msleep(500);
 	display.clear();
-	time.msleep(500);
+	msleep(500);
 	while (true) {
 		t = strftime("%H%M", new Date());
 		display.show(t);
 		display.set_colon((count & 1));
 		count += 1;
-		time.sleep(1);
+		sleep(1);
 	}
 }
 
