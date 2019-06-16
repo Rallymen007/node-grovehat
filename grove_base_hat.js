80628e28aa61a95aabd177577339a7bafb8dd94e
Stéphane Lavirotte <stephane@lavirotte.com>
2019-06-16 18:28:55 +0200

    Homogeneous naming convension

--- grove_base_hat.js
+++ grove_base_hat.js
@@ -1,7 +1,7 @@
 const Grove4DigitDisplay = require('./grove_4_digit_display.js');
 const GroveChainableRGBLed = require('./grove_chainable_rgb_led.js');
-const AnalogSensor = require('./adc.js');
+const GroveAnalogSensor = require('./adc.js');
 
 module.exports.Grove4DigitDisplay = Grove4DigitDisplay;
 module.exports.GroveChainableRGBLed = GroveChainableRGBLed;
-module.exports.AnalogSensor = AnalogSensor;
+module.exports.AnalogSensor = GroveAnalogSensor;
