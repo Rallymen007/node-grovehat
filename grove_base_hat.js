1a501753d9c82c3dcf43d3e083902ff346f7816d
Stéphane Lavirotte <stephane@lavirotte.com>
2019-06-16 16:29:42 +0200

    Specify relative file path

--- grove_base_hat.js
+++ grove_base_hat.js
@@ -1,5 +1,5 @@
-const Grove4DigitDisplay = require('grove_4_digit_display.js');
-const GroveChainableRGBLed = require('grove_chainable_rgb_led.js');
+const Grove4DigitDisplay = require('./grove_4_digit_display.js');
+const GroveChainableRGBLed = require('./grove_chainable_rgb_led.js');
 
 module.exports.Grove4DigitDisplay = Grove4DigitDisplay;
 module.exports.GroveChainableRGBLed = GroveChainableRGBLed;
